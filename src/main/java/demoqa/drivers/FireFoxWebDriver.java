44c3b5ef2760658100b4047d17799c40de8d0b89
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-12 00:09:40 +0600

    some ch

--- src/main/java/demoqa/drivers/FireFoxWebDriver.java
+++ src/main/java/demoqa/drivers/FireFoxWebDriver.java
@@ -14,24 +14,21 @@ public class FireFoxWebDriver {
     public static WebDriver loadFirefoxDriver() {
         System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver");
 
+        FirefoxOptions options = new FirefoxOptions();
+        // Добавьте ваши опции
+        if (Boolean.parseBoolean(ConfigReader.getValue("headless"))) {
+            options.addArguments("--headless");
+        }
 
-//        FirefoxOptions options = new FirefoxOptions();
-//        options.addArguments("--remote-allow-origin=*"); //
-//        options.addArguments("--disable-extensions"); // отключи какие-то плагины если есть
-//        options.addArguments("window-size=1920,1080");
-//        options.addArguments("no-sandbox");
-//        options.setPageLoadStrategy(PageLoadStrategy.NORMAL); // тут Используется по умолчанию, ожидает загрузки всех ресурсов.
-//        options.setPageLoadStrategy(PageLoadStrategy.EAGER); // Доступ к DOM готов, но другие ресурсы, например изображения, все еще могут загружаться.
-//        options.setPageLoadStrategy(PageLoadStrategy.NONE); // Совсем не блокирует WebDriver
-
-//        if (Boolean.parseBoolean(ConfigReader.getValue("headless"))) {
-//            options.addArguments("--headless");
-//        }
-
-
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        WebDriver driver = null;
+        try {
+            driver = new FirefoxDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Обработка ошибки
+        }
         return driver;
     }
 }
