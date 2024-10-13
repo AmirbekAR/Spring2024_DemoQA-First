53e94c963de28b153fc7c3fed0de971b3ca59d9e
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-13 21:17:13 +0600

    all changes2

--- src/test/java/demoqa_tests/WebTablesTest.java
+++ src/test/java/demoqa_tests/WebTablesTest.java
@@ -12,16 +12,16 @@ public class WebTablesTest extends BaseTest {
     public void webTableTest() throws InterruptedException {
         helper.browserManager.openURL("https://demoqa.com/webtables");
         demoqaPages.webTablesPage.addButton.click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         demoqaPages.webTablesPage.fillWebTablesForm(MockDataRegistrationForm.randomRegistration());
-        Thread.sleep(1000);
+        Thread.sleep(2000);
 
         demoqaPages.webTablesPage.editUser.click();
         demoqaPages.webTablesPage.fillWebTablesEditForm(MockDataRegistrationForm.randomRegistrationEdit());
         Thread.sleep(2000);
 
         demoqaPages.webTablesPage.deleteUser.click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
 
         demoqaPages.webTablesPage.searchBox.click();
         helper.webElementActions.input(demoqaPages.webTablesPage.searchBox, "Cierra");
