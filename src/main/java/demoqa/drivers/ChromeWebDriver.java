e830ac52844de1f5c401374addad2a65a5f0c75a
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-13 20:19:26 +0600

    some changes

--- src/main/java/demoqa/drivers/ChromeWebDriver.java
+++ src/main/java/demoqa/drivers/ChromeWebDriver.java
@@ -1,29 +1,29 @@
-package demoqa.drivers;
+        package demoqa.drivers;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+        import io.github.bonigarcia.wdm.WebDriverManager;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
+        import java.time.Duration;
 
-import static demoqa.utils.ConfigReader.getValue;
+        import static demoqa.utils.ConfigReader.getValue;
 
-public class ChromeWebDriver {
+        public class ChromeWebDriver {
 
-        public static WebDriver loadChromeDriver() {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--remote-allow-origins=*");
-                options.addArguments("--disable-extensions");
-                options.addArguments("--window-size-1920,1080");
-                options.addArguments("--no-sandbox");
-                if(Boolean.parseBoolean(getValue("headless"))) {
-                        options.addArguments("--headless");
+                public static WebDriver loadChromeDriver() {
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--remote-allow-origins=*");
+                        options.addArguments("--disable-extensions");
+                        options.addArguments("--window-size-1920,1080");
+                        options.addArguments("--no-sandbox");
+                        if(Boolean.parseBoolean(getValue("headless"))) {
+                                options.addArguments("--headless");
+                        }
+                        WebDriver driver = new ChromeDriver(options);
+                        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+                        driver.manage().window().maximize();
+                        return driver;
                 }
-                WebDriver driver = new ChromeDriver(options);
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-                driver.manage().window().maximize();
-                return driver;
         }
-}
