2a5dae0d4e59e526653e1d67b3a3f1a265425e09
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-24 23:14:13 +0600

    MMM

--- src/main/java/demoqa/drivers/ChromeWebDriver.java
+++ src/main/java/demoqa/drivers/ChromeWebDriver.java
@@ -1,10 +1,11 @@
         package demoqa.drivers;
 
-        import io.github.bonigarcia.wdm.WebDriverManager;
         import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.chrome.ChromeDriver;
         import org.openqa.selenium.chrome.ChromeOptions;
+        import org.openqa.selenium.remote.RemoteWebDriver;
 
+        import java.net.MalformedURLException;
+        import java.net.URL;
         import java.time.Duration;
 
         import static demoqa.utils.ConfigReader.getValue;
@@ -13,16 +14,21 @@
 
                 public static WebDriver loadChromeDriver() {
 
-                        WebDriverManager.chromedriver().setup();
                         ChromeOptions options = new ChromeOptions();
                         options.addArguments("--remote-allow-origins=*");
                         options.addArguments("--disable-extensions");
-                        options.addArguments("--window-size-1920,1080");
+                        options.addArguments("--window-size=1920,1080");
                         options.addArguments("--no-sandbox");
-                        if(Boolean.parseBoolean(getValue("headless"))) {
+                        if (Boolean.parseBoolean(getValue("headless"))) {
                                 options.addArguments("--headless");
                         }
-                        WebDriver driver = new ChromeDriver(options);
+                        // Specify the URL of the remote WebDriver server
+                        WebDriver driver = null;
+                        try {
+                                driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+                        } catch (MalformedURLException e) {
+                                throw new RuntimeException(e);
+                        }
                         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
                         driver.manage().window().maximize();
                         return driver;
