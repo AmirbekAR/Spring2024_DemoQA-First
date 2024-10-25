bd74d70acc411d1a44589d4cd62ba28170b31d17
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-25 19:25:11 +0600

    MM

--- src/main/java/demoqa/drivers/ChromeWebDriver.java
+++ src/main/java/demoqa/drivers/ChromeWebDriver.java
@@ -1,52 +1,35 @@
 package demoqa.drivers;
 
-import org.openqa.selenium.By;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.WebElement;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+import static demoqa.utils.ConfigReader.getValue;
+
 public class ChromeWebDriver {
 
-        public static WebDriver loadChromeDriver() {
+        public static WebDriver loadChromeDriver(){
+                WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
                 options.addArguments("--disable-extensions");
                 options.addArguments("--window-size=1920,1080");
                 options.addArguments("--no-sandbox");
-                options.addArguments("--headless"); // Если не нужен графический интерфейс
-                options.addArguments("--disable-gpu"); // Если используется headless режим
-
-                String hubUrl = "http://167.71.27.204:4444/wd/hub";
-
+                if (Boolean.parseBoolean(getValue("headless"))) {
+                        options.addArguments("--headless");
+                }
                 WebDriver driver = null;
                 try {
-                        driver = new RemoteWebDriver(new URL(hubUrl), options);
+                        driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
                 } catch (MalformedURLException e) {
-                        throw new RuntimeException("Invalid URL for RemoteWebDriver: " + e.getMessage(), e);
-                } catch (Exception e) {
-                        throw new RuntimeException("Error initializing WebDriver: " + e.getMessage(), e);
+                        throw new RuntimeException(e);
                 }
-
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
                 driver.manage().window().maximize();
                 return driver;
-        }
-
-        // Метод для клика по элементу с явным ожиданием
-        public static void clickElement(WebDriver driver, By by) {
-                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-                try {
-                        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(by));
-                        element.click();
-                } catch (Exception e) {
-                        throw new RuntimeException("Error clicking on element: " + e.getMessage(), e);
-                }
         }
 }
